fd983653e0cea9c308f6d4c5818e45f6507cc921
adambartyzal <adambartyzal@gmail.com>
2019-12-16 10:43:43 +0100

    Time now shows zero after countdown ends (maybe it should show time set)

---
+++ data/data.s
@@ -0,0 +1,10 @@
+@ Strigns in literal space, put after main loop 
+.equ ones,   0
+.equ tens,   1
+.align 4
+textReady: .asciz "READY   "
+textRun: .asciz   "RUN     "
+textSet: .asciz   "SET     "
+textTime: .asciz  "TIME = "
+textTimeSet: .asciz  "TIME SET = "
+.align 4
